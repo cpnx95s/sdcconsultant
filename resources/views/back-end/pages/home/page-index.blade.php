58f95b660d437da94dbf68886865e433ef2ba339
cpnx95s <55790886+cpnx95s@users.noreply.github.com>
2021-03-18 15:16:37 +0700

    ใส่ชาร์ตลงหน้าแดชบอร์ด

--- resources/views/back-end/pages/home/page-index.blade.php
+++ resources/views/back-end/pages/home/page-index.blade.php
@@ -1,15 +1,74 @@
 <div>
     <div class="fade-in">
         <div class="row">
-            <div class="col-md-2">
+            <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header bg-facebook content-center" style="height:100px;">
-                        <div class="c-icon c-icon-3xl text-white mb-4">
-                            <i class="fa fa-image"></i>
-                        </div>
-                    </div>
+                    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+                    
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<script>
+    window.onload = function() {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            theme: "light2",
+            title: {
+                text: "Simple Line Chart"
+            },
+            data: [{
+                type: "line",
+                indexLabelFontSize: 16,
+                dataPoints: [{
+                        y: 450
+                    },
+                    {
+                        y: 414
+                    },
+                    {
+                        y: 520,
+                        indexLabel: "\u2191 highest",
+                        markerColor: "red",
+                        markerType: "triangle"
+                    },
+                    {
+                        y: 460
+                    },
+                    {
+                        y: 450
+                    },
+                    {
+                        y: 500
+                    },
+                    {
+                        y: 480
+                    },
+                    {
+                        y: 480
+                    },
+                    {
+                        y: 410,
+                        indexLabel: "\u2193 lowest",
+                        markerColor: "DarkSlateGrey",
+                        markerType: "cross"
+                    },
+                    {
+                        y: 500
+                    },
+                    {
+                        y: 480
+                    },
+                    {
+                        y: 510
+                    }
+                ]
+            }]
+        });
+        chart.render();
+
+    }
+</script>
